fb97f568c6f53e969fd1010b55103ed97b0cad01
Rakeshmoond <2019uce1596@mnit.ac.in>
2021-07-23 06:21:18 +0530

    my website

---
+++ index.js
@@ -0,0 +1,29 @@
+const hamburger = document.querySelector(".header .nav-bar .nav-list .hamburger");
+const mobile_menu = document.querySelector(".header .nav-bar .nav-list ul");
+const header = document.querySelector(".header.container");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    mobile_menu.classList.toggle("active");
+});
+
+document.addEventListener("scroll", () => {
+    var scroll_position = window.scrollY;
+    if (scroll_position > 250) {
+        header.style.backgroundColor = "pink";
+    } else {
+        header.style.backgroundColor = "transparent";
+    }
+});
+
+const date = new Date();
+document.querySelector("#year").innerHTML = date.getFullYear();
+updateVisitCount()
+
+const countEl = document.querySelector("#count");
+function updateVisitCount() {
+    fetch("https://api.countapi.xyz/update/portfoliosite/count/?amount=1").then(res => res.json()).then(res => {
+        countEl.innerHTML = res.value;
+    })
+}
+
